1bf7837b629c8f208f6565aa668db1dc95db8970
Noman Masood <nomanmasood1234@gmail.com>
2020-08-18 12:40:39 -0400

    Changes to Files

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+const ReactCalendar = () => {
+  return <div> Hi! </div>
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
